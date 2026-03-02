b704c9fa44348f6514790d0a62b8a9cd5fa34c3e
Hossam Gamal <68028108+hossam-gamal22@users.noreply.github.com>
2026-03-02 11:47:27 +0400

    1

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, "node_modules"),
-];
-
-config.resolver.disableHierarchicalLookup = false;
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
