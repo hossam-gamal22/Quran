6b7202ca3f04264eba7e7d03eb641dce2f9d11bb
Hossam Gamal <68028108+hossam-gamal22@users.noreply.github.com>
2026-03-02 12:19:00 +0400

    1

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_enablePackageExports = true;
+
+// إضافة node_modules للـ watch folders
+config.watchFolders = [path.resolve(__dirname, "node_modules")];
+
+// إضافة extraNodeModules
+config.resolver.extraNodeModules = {
+  "expo-router": path.resolve(__dirname, "node_modules/expo-router"),
+  "@expo/metro-runtime": path.resolve(__dirname, "node_modules/@expo/metro-runtime"),
+};
 
 module.exports = config;
